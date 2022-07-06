f3c57c1ddec51a267849b666f1e70739bed643f3
Travis Brown <travisrobertbrown@gmail.com>
2022-07-06 16:26:19 +0200

    Add tests

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -49,6 +49,25 @@ impl Database {
         })
     }
 
+    pub fn pairs(&self) -> accounts::PairIterator<rocksdb::DBIterator> {
+        self.accounts.pairs()
+    }
+
+    pub fn get_counts(
+        &self,
+    ) -> Result<
+        (
+            accounts::AccountTableCounts,
+            screen_names::ScreenNameTableCounts,
+        ),
+        Error,
+    > {
+        let account_counts = self.accounts.get_counts()?;
+        let screen_name_counts = self.screen_names.get_counts()?;
+
+        Ok((account_counts, screen_name_counts))
+    }
+
     pub fn lookup_by_user_id(
         &self,
         user_id: u64,
@@ -81,5 +100,78 @@ impl Database {
 
     pub fn compact_ranges(&self) -> Result<(), Error> {
         self.accounts.compact_ranges()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::HashMap;
+
+    #[test]
+    fn insert() {
+        let dir = tempfile::tempdir().unwrap();
+        let db = Database::open(dir).unwrap();
+        db.insert(123, "foo", vec![]).unwrap();
+        db.insert(123, "bar", vec![]).unwrap();
+        db.insert(456, "foo", vec![]).unwrap();
+        db.insert(123, "foo", vec![]).unwrap();
+
+        let mut expected_by_id = HashMap::new();
+        expected_by_id.insert("foo".to_string(), vec![]);
+        expected_by_id.insert("bar".to_string(), vec![]);
+
+        let expected_pairs = vec![
+            (123, "bar".to_string(), vec![]),
+            (123, "foo".to_string(), vec![]),
+            (456, "foo".to_string(), vec![]),
+        ];
+
+        let expected_counts = (
+            accounts::AccountTableCounts { id_count: 2, pair_count: 3 },
+            screen_names::ScreenNameTableCounts { screen_name_count: 2, mapping_count: 3 }
+        );
+
+
+        assert_eq!(db.lookup_by_screen_name("foo").unwrap(), vec![123, 456]);
+        assert_eq!(db.lookup_by_user_id(123).unwrap(), expected_by_id);
+        assert_eq!(db.get_counts().unwrap(), expected_counts);
+        assert_eq!(
+            db.pairs().collect::<Result<Vec<_>, _>>().unwrap(),
+            expected_pairs
+        );
+
+        db.compact_ranges().unwrap();
+
+        assert_eq!(db.lookup_by_screen_name("foo").unwrap(), vec![123, 456]);
+        assert_eq!(db.lookup_by_user_id(123).unwrap(), expected_by_id);
+        assert_eq!(db.get_counts().unwrap(), expected_counts);
+        assert_eq!(
+            db.pairs().collect::<Result<Vec<_>, _>>().unwrap(),
+            expected_pairs
+        );
+    }
+
+    #[test]
+    fn lookup_by_screen_name_prefix() {
+        let dir = tempfile::tempdir().unwrap();
+        let db = Database::open(dir).unwrap();
+        db.insert(123, "foo", vec![]).unwrap();
+        db.insert(123, "bar", vec![]).unwrap();
+        db.insert(1000, "for", vec![]).unwrap();
+        db.insert(1001, "baz", vec![]).unwrap();
+        db.insert(1002, "follow", vec![]).unwrap();
+        db.insert(1003, "FOR", vec![]).unwrap();
+
+        let expected = vec![
+            ("follow".to_string(), vec![1002]),
+            ("foo".to_string(), vec![123]),
+            ("for".to_string(), vec![1000, 1003]),
+        ];
+
+        assert_eq!(
+            db.lookup_by_screen_name_prefix("fo", 128).unwrap(),
+            expected
+        );
     }
 }
