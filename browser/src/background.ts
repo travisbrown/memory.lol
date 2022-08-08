af064aaf1f2825e6cabcf847f5486feba1f19b26
Travis Brown <travisrobertbrown@gmail.com>
2022-08-08 15:33:43 +0200

    Present info for deactivated or suspended accounts

--- browser/src/background.ts
+++ browser/src/background.ts
@@ -1,11 +1,21 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  fetch(`https://api.memory.lol/v1/tw/id/${request.id}`, {
-    method: "get",
-    headers: { "Content-Type": "application/json" },
-    credentials: "same-origin",
-  })
-    .then((response) => response.json())
-    .then((data) => sendResponse({ result: data.screen_names }));
+  if (request.id) {
+    fetch(`https://api.memory.lol/v1/tw/id/${request.id}`, {
+      method: "get",
+      headers: { "Content-Type": "application/json" },
+      credentials: "same-origin",
+    })
+      .then((response) => response.json())
+      .then((data) => sendResponse({ result: data.screen_names }));
+  } else if (request.screenName) {
+    fetch(`https://api.memory.lol/v1/tw/${request.screenName}`, {
+      method: "get",
+      headers: { "Content-Type": "application/json" },
+      credentials: "same-origin",
+    })
+      .then((response) => response.json())
+      .then((data) => sendResponse({ result: data.accounts }));
+  }
 
   return true;
 });
