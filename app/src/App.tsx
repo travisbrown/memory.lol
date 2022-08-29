070e4cd787e3573d7b9d20eb6a83ce5cd42e49d8
Travis Brown <travisrobertbrown@gmail.com>
2022-08-29 18:10:00 +0200

    Logo, etc. (#28)
    * Add basic icon and logo
    
    * Miscellaneous interface improvements

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,40 +1,105 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Navbar, Section } from "react-bulma-components";
+import {
+  Container,
+  Content,
+  Footer,
+  Heading,
+  Navbar,
+  Section,
+  Tile,
+} from "react-bulma-components";
 
-import "bulma/css/bulma.min.css";
 import "./App.css";
 import { LoginStatus } from "./LoginStatus";
 import { PostLoginRedirect } from "./PostLoginRedirect";
+import { TwForm } from "./TwForm";
 import { TwSearch } from "./TwSearch";
+import React from "react";
 
 function App() {
   return (
     <BrowserRouter basename="/app">
       <PostLoginRedirect />
-      <Navbar aria-label="main navigation">
+      <Navbar aria-label="main navigation" px="6">
         <Navbar.Brand>
-          <Navbar.Item href="https://memory.lol/">memory.lol</Navbar.Item>
+          <Navbar.Item href="https://memory.lol/">
+            <img alt="memory.lol" src="/app/logos/dumpster.svg" />
+          </Navbar.Item>
         </Navbar.Brand>
-        {/*<Navbar.Container align='left'>
-            <Navbar.Item active>home</Navbar.Item>
-            <Navbar.Item>documentation</Navbar.Item>
-            <Navbar.Dropdown>
-              <Navbar.Item href="/about">About</Navbar.Item>
-              <Navbar.Divider />
-              <Navbar.Item href="/report">Report an issue</Navbar.Item>
-            </Navbar.Dropdown>
-        </Navbar.Container>*/}
+        <Navbar.Container align="left">
+          <Navbar.Item href="/app/" active>
+            Home
+          </Navbar.Item>
+          <Navbar.Item href="https://github.com/travisbrown/memory.lol">
+            About
+          </Navbar.Item>
+        </Navbar.Container>
         <Navbar.Container align="right">
           <LoginStatus />
         </Navbar.Container>
       </Navbar>
       <Section>
+        <Tile kind="ancestor">
+          <Tile kind="parent">
+            <Tile kind="child" renderAs="article" px="4" pb="4">
+              <Heading subtitle>Welcome to memory.lol</Heading>
+              <Content pb="4">
+                <p>
+                  This site is an instance of software from the{" "}
+                  <a href="https://github.com/travisbrown/hassreden-tracker">
+                    Hassreden-Tracker{" "}
+                  </a>
+                  project. Search results are limited for unauthenticated users
+                  (see{" "}
+                  <a href="https://github.com/travisbrown/memory.lol#current-access-restrictions">
+                    this document
+                  </a>{" "}
+                  for details).
+                </p>
+                <p>
+                  Please contact us{" "}
+                  <a href="mailto:travisrobertbrown@protonmail.com">by email</a>{" "}
+                  to discuss trusted access.
+                </p>
+              </Content>
+            </Tile>
+            <Tile kind="child" px="4" pb="4">
+              <Heading subtitle>Twitter history search</Heading>
+              <Container>
+                <TwForm />
+              </Container>
+            </Tile>
+          </Tile>
+        </Tile>
         <Routes>
           <Route path="/" />
           <Route path="/tw/id/:userId" element={<TwSearch />} />
           <Route path="/tw/:screenName" element={<TwSearch />} />
         </Routes>
       </Section>
+      <Footer>
+        <Container>
+          <Content style={{ textAlign: "center" }}>
+            <p>
+              <a href="https://github.com/travisbrown/hassreden-tracker">
+                <strong>Hassreden-Tracker</strong>
+              </a>{" "}
+              and{" "}
+              <a href="https://memory.lol/app/">
+                <strong>memory.lol</strong>
+              </a>{" "}
+              are developed by{" "}
+              <a href="https://twitter.com/travisbrown">Travis Brown</a>. The
+              source code is licensed under the
+              <a href="https://anticapitalist.software/">
+                {" "}
+                Anti-Capitalist Software License
+              </a>
+              .
+            </p>
+          </Content>
+        </Container>
+      </Footer>
     </BrowserRouter>
   );
 }
---
+++ app/src/TwForm.tsx
@@ -0,0 +1,73 @@
+import { Button, Form } from "react-bulma-components";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+export function TwForm() {
+  const navigate = useNavigate();
+  const [searchFields, setSearchFields] = useState({
+    screen_name: "",
+    twitter_id: "",
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchFields({
+      ...searchFields,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleScreenNameSearch = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+    if (searchFields.screen_name !== "") {
+      navigate(`tw/${searchFields.screen_name}`);
+    }
+  };
+
+  const handleTwitterIdSearch = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+    if (searchFields.twitter_id !== "") {
+      navigate(`tw/id/${searchFields.twitter_id}`);
+    }
+  };
+
+  return (
+    <form>
+      <Form.Field kind="addons">
+        <Form.Control>
+          <Form.Input
+            placeholder="Screen name"
+            type="text"
+            name="screen_name"
+            value={searchFields.screen_name}
+            onChange={handleChange}
+          />
+        </Form.Control>
+        <Form.Control>
+          <Button color="info" onClick={handleScreenNameSearch}>
+            Search
+          </Button>
+        </Form.Control>
+      </Form.Field>
+      <Form.Field kind="addons">
+        <Form.Control>
+          <Form.Input
+            placeholder="Twitter ID"
+            type="text"
+            name="twitter_id"
+            value={searchFields.twitter_id}
+            onChange={handleChange}
+          />
+        </Form.Control>
+        <Form.Control>
+          <Button color="info" onClick={handleTwitterIdSearch}>
+            Search
+          </Button>
+        </Form.Control>
+      </Form.Field>
+    </form>
+  );
+}
